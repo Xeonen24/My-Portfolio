ea3632c69031727bb38464c50bbc1fad45411e3d
Xeonen24 <captainsparkelz32@gmail.com>
2023-05-03 11:57:58 +0530

    ehe?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Resume from './Components/Resume/Resume'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <Routes>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/Contact' component={Contact} />
+      <Route exact path='/Resume' component={Resume} />
+    </Routes>
+   </Router>
   );
 }
 
